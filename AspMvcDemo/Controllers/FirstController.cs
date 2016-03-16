b2b8d93ad76dd217e21d8c69ca9b3e354b9d25d1
ananda tiwari <anandatiwari31@gmail.com>
2016-03-16 23:48:53 +0545

    First Commit

---
+++ AspMvcDemo/Controllers/FirstController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AspMvcDemo.Controllers
+{
+    public class FirstController : Controller
+    {
+        // GET: First
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Home()
+        {
+            Models.FirstModel.first objcon = new Models.FirstModel.first();
+            List<Models.FirstModel.first> Li = objcon.getdata(0,"","");
+            return View("home", Li.ToList());
+        }
+        public ActionResult getdata()
+        {
+            Models.FirstModel.first objcon = new Models.FirstModel.first();
+            List<Models.FirstModel.first> Li = objcon.getdata(0, "", "");
+            return View("home", Li.ToList());
+        }
+        public ActionResult editdata(int id, string name, string address)
+        {
+            Models.FirstModel.first obj = new Models.FirstModel.first();
+            List<Models.FirstModel.first> Li = obj.getdata(id, name, address);
+            return View("editData",Li.ToList());
+        }
+        public ActionResult savedata(string name, string address)
+        {
+            int id = Convert.ToInt32(Session["id"].ToString());
+            Models.FirstModel.first obj = new Models.FirstModel.first();
+            obj.updatedata(id, name, address);
+            List<Models.FirstModel.first> Li = obj.getdata(0, "","");
+            return View("home", Li.ToList());
+        }
+        public ActionResult deletedata(int id)
+        {
+            Models.FirstModel.first obj = new Models.FirstModel.first();
+            obj.deletedata(id);
+            List<Models.FirstModel.first> Li = obj.getdata(0, "","");
+
+            return RedirectToAction("home", "first", new { id = 0 });
+        }
+        public ActionResult adddata()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult insertdata(string name = "", string address = "")
+        {
+            Models.FirstModel.first obj = new Models.FirstModel.first();
+            obj.insertdata(name, address);
+            List<Models.FirstModel.first> Li = obj.getdata(0, "", "");
+            return View("home", Li.ToList());
+        }
+    }
+}
---
+++ AspMvcDemo/Models/FirstModel.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.Data.SqlClient;
+using System.Data;
+
+namespace AspMvcDemo.Models
+{
+    public class FirstModel
+    {
+        public class first {
+            SqlConnection con = new SqlConnection("Data Source=(local);database=mvctest;user=YourUserName;password=YourPassword");
+            public int id { get; set; }
+            public string name { get; set; }
+            public string address { get; set; }
+
+            public List<first> getdata(int id, string name, string address) {
+                if (id.ToString() == null) { id = 0; }
+                List<first> objlist = new List<first>(5000);
+                if (con.State == ConnectionState.Broken || con.State == ConnectionState.Closed ) {
+                    con.Open();
+                }
+                string smt = "";
+                if (id == 0)
+                {
+                    smt = "select id, name, address from mvcdemo";
+                }
+                else
+                {
+                    smt = "select id, name, address from mvcdemo where id='"+ id +"'";
+                }
+                SqlDataAdapter da = new SqlDataAdapter(smt, con);
+                DataSet ds = new DataSet();
+                da.Fill(ds, "Table");
+                int i = 0;
+                while (i<ds.Tables[0].Rows.Count) {
+                    first obj1 = new first();
+                    obj1.id = Convert.ToInt32(ds.Tables[0].Rows[i]["id"]);
+                    obj1.name = Convert.ToString(ds.Tables[0].Rows[i]["name"]);
+                    obj1.address = Convert.ToString(ds.Tables[0].Rows[i]["address"]);
+
+                    objlist.Add(obj1);
+                    i++;
+                }
+                return objlist;
+            }
+            public void updatedata(int id, string name, string address)
+            {
+                if (con.State == ConnectionState.Broken || con.State == ConnectionState.Closed)
+                {
+
+                    con.Open();
+                }
+                string smt = "Update mvcdemo set name = '" + name + "', address = '" + address + "' where id ='" + id + "'";
+                SqlCommand cmd = new SqlCommand(smt, con);
+                cmd.ExecuteNonQuery();
+
+            }
+            public void deletedata(int id)
+            {
+                if (con.State == ConnectionState.Broken || con.State == ConnectionState.Closed)
+                {
+
+                    con.Open();
+                }
+                string smt = "delete from mvcdemo where id =  " + id + "";
+                SqlCommand cmd = new SqlCommand(smt, con);
+                cmd.ExecuteNonQuery();
+
+            }
+            public void insertdata(string name, string address) {
+                if (con.State == ConnectionState.Broken || con.State == ConnectionState.Closed) {
+                    con.Open();
+                }
+                string smt = "insert into mvcdemo values ('" + name + "','" + address + "')";
+                SqlCommand cmd = new SqlCommand(smt, con);
+                cmd.ExecuteNonQuery();
+            }
+
+        }
+    }
+}
